5643897680e880606f825fe940cc0dae96bf10f0
Kariuki Kihikah <kariukikihikah@gmail.com>
2025-10-18 18:52:42 +0300

    Configure navigations

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,46 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
 import Team from "./components/Team";
 import Stats from "./components/StatsSection";
 import PartnersSection from "./components/PartnersSection";
 import Footer from "./components/Footer";
-//import BusinessProcesses from "./components/BusinessProcesses";
+
+// Pages
+import OurBackground from "./pages/OurBackground";
+import WhyWorkWithUs from "./pages/WhyWorkWithUs";
+// import BusinessPolicies from "./pages/BusinessPolicies";
 
 function App() {
   return (
-    <div className="app-container">
-      <Header />
-      <main className="main-content">
-        <Hero />
-        <Team />
-        <Stats />
-      <PartnersSection />
-      </main>
-      <Footer />
-      {/* <BusinessProcesses /> */}
-    </div>
+    <Router>
+      <div className="app-container">
+        <Header />
+        <main className="main-content">
+          <Routes>
+            {/* Home page */}
+            <Route
+              path="/"
+              element={
+                <>
+                  <Hero />
+                  <Team />
+                  <Stats />
+                  <PartnersSection />
+                </>
+              }
+            />
+
+            {/* About pages */}
+            <Route path="/about/background" element={<OurBackground />} />
+            <Route path="/about/why-work-with-us" element={<WhyWorkWithUs />} />
+            {/* <Route path="/about/business-policies" element={<BusinessPolicies />} /> */}
+          </Routes>
+        </main>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
